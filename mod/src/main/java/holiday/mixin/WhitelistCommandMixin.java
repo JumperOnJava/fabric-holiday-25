0d7a228ddd23863a116513c4d72909ee64472716
LouisNotLuna <louis@trans.fish>
2025-12-13 10:16:02 +0100

    Add Whitelist Executablity

--- mod/src/main/java/holiday/mixin/WhitelistCommandMixin.java
+++ mod/src/main/java/holiday/mixin/WhitelistCommandMixin.java
@@ -1,27 +1,38 @@
 package holiday.mixin;
 
+import com.mojang.brigadier.builder.ArgumentBuilder;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import holiday.WhitelistLogger;
 import net.minecraft.server.PlayerConfigEntry;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.dedicated.command.WhitelistCommand;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Collection;
+import java.util.function.Predicate;
 
+@Debug(export = true)
 @Mixin(WhitelistCommand.class)
 public class WhitelistCommandMixin {
     @Inject(
             method = "executeAdd",
             at = @At("HEAD")
     )
-    private static void a(ServerCommandSource source, Collection<PlayerConfigEntry> targets, CallbackInfoReturnable<Integer> cir) {
+    private static void logAdds(ServerCommandSource source, Collection<PlayerConfigEntry> targets, CallbackInfoReturnable<Integer> cir) {
         var whitelister = source.getName();
         targets.forEach(whiteliste -> {
             var whitelisteName = whiteliste.name();
             WhitelistLogger.logWhitelisting(whitelister, whitelisteName);
         });
+    }
+
+    @Redirect(
+            method = "register", at = @At(value = "INVOKE", target = "Lcom/mojang/brigadier/builder/LiteralArgumentBuilder;requires(Ljava/util/function/Predicate;)Lcom/mojang/brigadier/builder/ArgumentBuilder;")
+    )
+    private static ArgumentBuilder makeExecutableForEveryone(LiteralArgumentBuilder instance, Predicate predicate) {
+        return instance;
     }
 }
