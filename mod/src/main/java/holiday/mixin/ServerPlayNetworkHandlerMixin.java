bf6e8c27064faab78a9d43cd8eb6be93d221b85e
LouisNotLuna <louis@trans.fish>
2025-12-17 20:37:22 +0100

    Fix bug

--- mod/src/main/java/holiday/mixin/ServerPlayNetworkHandlerMixin.java
+++ mod/src/main/java/holiday/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,6 +1,8 @@
 package holiday.mixin;
 
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -11,13 +13,15 @@ import net.minecraft.server.network.ServerPlayNetworkHandler;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin {
+    @Shadow public ServerPlayerEntity player;
+
     @Inject(
             method = "getMaxAllowedFloatingTicks",
             at = @At("HEAD"),
             cancellable = true
     )
     private void makePlayerAbsolutelySafeFromKickedForFlying(CallbackInfoReturnable<Integer> ci) {
-        if (HolidayServerItems.isAbsolutelySafe((LivingEntity) (Object) this)) {
+        if (HolidayServerItems.isAbsolutelySafe(this.player)) {
             ci.setReturnValue(Integer.MAX_VALUE);
         }
     }
