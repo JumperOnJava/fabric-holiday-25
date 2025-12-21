ed0ec965ccb05f3ade2399005d964a8e05debb27
haykam821 <24855774+haykam821@users.noreply.github.com>
2025-12-21 04:50:45 -0500

    Prevent hopper mites from being transferred through hoppers

--- mod/src/main/java/holiday/mixin/HopperBlockEntityMixin.java
+++ mod/src/main/java/holiday/mixin/HopperBlockEntityMixin.java
@@ -4,11 +4,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import holiday.item.HopperMiteItem;
+import holiday.tag.HolidayServerItemTags;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.HopperBlockEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 @Mixin(HopperBlockEntity.class)
@@ -19,5 +24,27 @@ public class HopperBlockEntityMixin {
     )
     private static void applyHopperMiteEffects(World world, BlockPos pos, BlockState state, HopperBlockEntity blockEntity, CallbackInfo ci) {
         HopperMiteItem.applyEffectsTo(world, pos, blockEntity);
+    }
+
+    @Inject(
+            method = "canInsert",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private static void preventInsertingHopperTrapped(Inventory inventory, ItemStack stack, int slot, Direction side, CallbackInfoReturnable<Boolean> ci) {
+        if (stack.isIn(HolidayServerItemTags.HOPPER_TRAPPED)) {
+            ci.setReturnValue(false);
+        }
+    }
+
+    @Inject(
+            method = "canExtract",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private static void preventExtractingHopperTrapped(Inventory hopperInventory, Inventory fromInventory, ItemStack stack, int slot, Direction facing, CallbackInfoReturnable<Boolean> ci) {
+        if (stack.isIn(HolidayServerItemTags.HOPPER_TRAPPED)) {
+            ci.setReturnValue(false);
+        }
     }
 }
