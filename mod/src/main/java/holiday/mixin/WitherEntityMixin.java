599c46b098b057bbd1cc7c29fe9399ff7fad3769
tenneb22 <126985099+Tenneb22@users.noreply.github.com>
2025-12-27 22:21:28 +0100

    do not explode when spawning in overworld

--- mod/src/main/java/holiday/mixin/WitherEntityMixin.java
+++ mod/src/main/java/holiday/mixin/WitherEntityMixin.java
@@ -4,12 +4,14 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import holiday.entity.HeartEntity;
 import holiday.idkwheretoputthis.WitherEntityExtension;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.boss.WitherEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.projectile.WitherSkullEntity;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldEvents;
@@ -60,7 +62,7 @@ public abstract class WitherEntityMixin extends HostileEntity implements WitherE
 
         Vec3d vec3d = new Vec3d(g, h, i);
 
-        if (this.getEntityWorld().getDimensionEntry().matchesKey(DimensionTypes.OVERWORLD)) {
+        if (fabric_holiday_25$isInOverWorld()) {
             HeartEntity heartEntity = new HeartEntity(this.getEntityWorld(), this, vec3d.normalize());
             //heartEntity.setOwner(this);
             heartEntity.setPosition(d, e, f);
@@ -87,10 +89,23 @@ public abstract class WitherEntityMixin extends HostileEntity implements WitherE
             target = "Lnet/minecraft/world/World;addParticleClient(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V")
     )
     private void wrapAddParticleClient(World instance, ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ, Operation<Void> original) {
-        if (instance.getDimensionEntry().matchesKey(DimensionTypes.OVERWORLD)) {
+        if (fabric_holiday_25$isInOverWorld()) {
             original.call(instance, ParticleTypes.HEART, x, y, z, velocityX, velocityY, velocityZ);
         } else {
             original.call(instance, parameters, x, y, z, velocityX, velocityY, velocityZ);
+        }
+    }
+    
+    @WrapOperation(
+        method = "mobTick",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/server/world/ServerWorld;createExplosion(Lnet/minecraft/entity/Entity;DDDFZLnet/minecraft/world/World$ExplosionSourceType;)V"
+        )
+    )
+    private void wrapCreateExplosion(ServerWorld instance, Entity entity, double x, double y, double z, float power, boolean b, World.ExplosionSourceType explosionSourceType, Operation<Void> original) {
+        if (!fabric_holiday_25$isInOverWorld()) {
+            original.call(instance, entity, x, y, z, power, b, explosionSourceType);
         }
     }
 
@@ -104,7 +119,7 @@ public abstract class WitherEntityMixin extends HostileEntity implements WitherE
         )
     )
     private int overrideCooldown(WitherEntity instance) {
-        if (instance.getEntityWorld().getDimensionEntry().matchesKey(DimensionTypes.OVERWORLD)) {
+        if (fabric_holiday_25$isInOverWorld()) {
             return 0;
         }
         return this.blockBreakingCooldown;
